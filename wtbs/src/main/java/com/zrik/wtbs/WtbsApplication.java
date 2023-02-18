244921a0a318eb82d11948b76e41c49636bb592c
void-kun <hoangzrik@gmail.com>
2023-02-18 14:27:22 +0700

    Init project worldtime

---
+++ wtbs/src/main/java/com/zrik/wtbs/WtbsApplication.java
@@ -0,0 +1,13 @@
+package com.zrik.wtbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WtbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WtbsApplication.class, args);
+    }
+
+}
---
+++ wtbs/src/test/java/com/zrik/wtbs/WtbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zrik.wtbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WtbsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
